e4b01118af0867d15ece60340a0d4fe33b684cba
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-10-18 13:40:22 +0500

    Add files via upload

--- src/ThrowSceneEvent.java
+++ src/ThrowSceneEvent.java
@@ -1,21 +1,25 @@
 package fps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThrowSceneEvent {
 	private SceneEventListener glistener;
+	private List<SceneEventListener> listeners = new ArrayList<SceneEventListener>();
 	  public  void addEventListener(SceneEventListener listener)  {
-		  glistener=listener;
+		  //glistener=listener;
+		  listeners.add(listener);
 	  }
-	 // public synchronized void removeEventListener(SceneEventListener listener)   {
-	  //  _listeners.remove(listener);
-	  //}
 	 
-	  // call this method whenever you want to notify
-	  //the event listeners of the particular event
 	  public void fireEvent(SceneControlData message){
 		  SceneEvent event = new SceneEvent(this,message );
-	    
+		 
+		  
+		  for (SceneEventListener hl : listeners) {
+	            hl.handleSceneEvent(event);
+	    }
 	   
-	    	glistener.handleSceneEvent(event);
+	    	
 	    
 	  }
 
