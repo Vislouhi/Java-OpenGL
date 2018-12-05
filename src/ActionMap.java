ce81c0f7c037e0b041a51c1350e5bcee4a643149
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-12-05 15:19:40 +0600

    Update ActionMap.java

--- src/ActionMap.java
+++ src/ActionMap.java
@@ -1,6 +1,7 @@
 public class ActionMap{
 
  ArrayList<Action> actionList;
+ Tab tab;
  
  public void addAction(Color color,Action action)
  {
@@ -12,9 +13,66 @@ public class ActionMap{
  
  }
  
- private class Action{
+}
+ public class Action{
+ String actionType ="";//Может принимать значения button, edit.
  
+ EditForm editForm;
+ TabButton tabButton;
+ Tab tab;
+  
+  public Action(Tab tab)
+  {
+  
+  }
+  
+  
+  private void setActionColor(Color color)
+  {
+  
+  }
+  
+  private void setActionType(String actionType){
+   
+  }
+  
+  private void performAction()
+  {
+   //В зависимости от значения type
+    tab.executeButtonScript();
+    tab.focusOnEditForm();
+    
+  }
  }
 
-
+public class EditForm extends RenderForm
+{
+ String text;
+ int availableTextSize;
+ 
+ public void setText()
+ {
+ 
+ }
+ 
+ public void setAvailableTextSize()
+ {
+ 
+ }
+ 
+ public String getText()
+ {
+ 
+ }
+ 
 }
+
+public class Page{
+ Texture texture;
+ ArrayList<EditForm> editForms;
+ ArrayList<TabButton> tabButtons;
+ 
+}
+
+
+
