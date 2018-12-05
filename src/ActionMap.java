49315f5457d15d7e5a70b847f95fd49c96f6832d
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-12-05 14:48:48 +0600

    Create ActionMap.java

---
+++ src/ActionMap.java
@@ -0,0 +1,20 @@
+public class ActionMap{
+
+ ArrayList<Action> actionList;
+ 
+ public void addAction(Color color,Action action)
+ {
+ 
+ }
+ 
+ public Action getAction(Color color)
+ {
+ 
+ }
+ 
+ private class Action{
+ 
+ }
+
+
+}
