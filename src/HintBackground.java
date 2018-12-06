6ad27d9a3aecfe3b49b55c90f438ac581d62b2b4
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-12-06 14:03:38 +0600

    Update HintBackground.java

--- src/HintBackground.java
+++ src/HintBackground.java
@@ -1,1 +1,25 @@
 
+public class HintBackground{
+  HUDItem hudItem;
+  boolean active=true;
+  
+  public void setHudItem()
+  {
+   hudItem = new HUDItem();
+  }
+  
+  public void activate(){
+    this.active=true;
+  }
+  
+   public void disactivate(){
+    this.active=false;
+  }
+  
+  public boolean isActive()
+  {
+  return this.active;
+  }
+  
+  
+}
