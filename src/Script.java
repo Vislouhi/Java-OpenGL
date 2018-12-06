eeadef10b6ce7ae5fd2a7a3910e3ded2e9868a0b
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-12-06 14:12:00 +0600

    Update Script.java

--- src/Script.java
+++ src/Script.java
@@ -1,1 +1,19 @@
 
+public class Script 
+{
+ private JSON script;
+  private RigidBody rigidBody;
+  private RigidBodyEmmiter rigidBodyEmmiter;
+  
+ public Script(ArrayList<JSON> script, MatchParser mathParser)
+ {
+  this.script=script;  
+   
+ }
+  
+  public void runScript()
+  {
+  
+  }
+  
+}
